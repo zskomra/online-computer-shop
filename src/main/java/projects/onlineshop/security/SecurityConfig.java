fcbde4bd48029db0d7f2d44817a846f55f4e43d8
z.skomra <z.skomraa@gmail.com>
2020-12-06 12:02:44 +0100

    #5our :: add user registration

---
+++ src/main/java/projects/onlineshop/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package projects.onlineshop.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/register").permitAll()
+                .mvcMatchers("/login").permitAll()
+                .anyRequest().authenticated();
+    }
+}
